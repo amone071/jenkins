9e0dce68b355eb7ac85ebe33741d5f8b0446e1be
Akash <amone071@uottawa.ca>
2021-05-13 20:50:40 -0400

    java hello

---
+++ src/SessionFive.java
@@ -0,0 +1,19 @@
+public class SessionFive {
+    // Strings
+    public static void main(String[] args) {
+
+
+        String str1 = "Java";
+        char[] chars = {'H', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd'};
+
+        String str2 = new String(chars);
+        System.out.println(str1);
+        System.out.println(str2);
+
+        String H = "Hello";
+        String H1 = "hello";
+
+        int compare = H.compareTo(H1);
+        System.out.println(compare);
+    }
+}
